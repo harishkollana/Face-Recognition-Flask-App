802bd488315c169dc3ff326b52c4d31078bb2a4f
Harish K <59809982+harishkollana@users.noreply.github.com>
2022-03-19 18:34:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from flask import Flask, render_template, Response
 import cv2
 import numpy as np
-from tensorflow import keras
-from tensorflow.keras.models import model_from_json  
+from tensorflow import keras 
 from tensorflow.keras.preprocessing import image  
 
-model = keras.models.load_model('Final_model_Custome_CNN.h5')  #load model  
+#load model  
+model = keras.models.load_model('Final_model_Custome_CNN.h5') 
 
+#load weights  
 model.load_weights('Final_model_Custome_CNN.h5')                            #load weights  
 
 face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-camera = cv2.VideoCapture(0)
 
 app = Flask(__name__)
+
+camera = cv2.VideoCapture(0)
 
 
 def gen_frames():                                       # generate frame by frame from camera
@@ -25,24 +27,29 @@ def gen_frames():                                       # generate frame by fram
         else:
             gray_img= cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  
         
-            faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.3, 1)  
+            faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)  
             
         
             for (x,y,w,h) in faces_detected:
                 
-                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),thickness=2)  
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),thickness=7)  
                 roi_gray=gray_img[y:y+w,x:x+h]          #cropping region of interest i.e. face area from  image  
                 roi_gray=cv2.resize(roi_gray,(48,48))  
                 img_pixels = image.img_to_array(roi_gray)  
                 img_pixels = np.expand_dims(img_pixels, axis = 0)  
                 img_pixels /= 255  
+
+                print(img_pixels.shape)
         
-                predictions = model.predict(img_pixels)  
+                predictions = model.predict(img_pixels) 
+
+                #find max indexed array  
         
                 max_index = np.argmax(predictions[0])   #find max indexed array
         
                 emotions = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']  
                 predicted_emotion = emotions[max_index]  
+                print(predicted_emotion)
                 
                 cv2.putText(frame, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)  
         
@@ -66,4 +73,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
